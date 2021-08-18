f452147dc80859b4e6ca04495627704cc42f6f80
afrizzal <apratama8@gmail.com>
2021-08-18 08:05:17 +0700

    sync ver

--- index.js
+++ index.js
@@ -2,18 +2,9 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'Integ123';
 
-jwt.sign({data: { kelas: 'ecomm'}}, JWT_SECRET, { expiresIn: 1600}, (err, token) => {
-    console.log(token);
-});
+//create token sync
 
-const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImtlbGFzIjoiZWNvbW0ifSwiaWF0IjoxNjI5MTE4OTI0fQ.pOsbhUixpiIj-_jvj12y3bdRHSbEeZzl2nnPEhGXBLc';
-jwt.verify(token1, JWT_SECRET, (err, decoded) => {
-    if (err) {
-        console.log(err.message);
-        return;
-    }
-    console.log(decoded);
-});
-
-// console.log('qwertyuiop');
-
+const token = jwt.sign({ data: {kelas: 'ecomm'} }, 
+JWT_SECRET,
+{ expiresIn: '15m' });
+// console.log(token);
